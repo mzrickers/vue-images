da97682027410fd6a0502f8f353e75a2caf19f43
Zach Rickers <mzrickers@gmail.com>
2020-08-31 12:25:39 -0600

    Communicating across modules

--- src/api/imgur.js
+++ src/api/imgur.js
@@ -1,5 +1,6 @@
 import { CLIENT_ID } from '../../imgurCreds';
 import qs from 'qs';
+import axios from 'axios';
 
 const ROOT_URL = 'https://api.imgur.com';
 
@@ -11,5 +12,12 @@ export default {
         };
 
         window.location = `${ROOT_URL}/oauth2/authorize?${qs.stringify(querystring)}`
+    },
+    fetchImages(token) {
+        return axios.get(`${ROOT_URL}/3/account/me/images`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
     }
 }
